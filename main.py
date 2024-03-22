8730de5bcd76c332ca14cae856a9123e16700230
nicola1223 <belchuk2005@mail.ru>
2024-03-22 22:30:38 +0300

    Added parameters

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="App"
+)
 
 
-@app.get("/")
-def get_hello():
-    return "Hello, world!"
+fake_users = [
+    {"id": 1, "role": "admin", "name": "Nick"},
+    {"id": 2, "role": "moderator", "name": "Anna"},
+    {"id": 3, "role": "user", "name": "John"}
+]
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get("id") == user_id]
+
+
+fake_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 321, "amount": 2.12},
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "sell": 321, "amount": 2.12}
+]
+
+
+@app.get("/trades")
+def get_trades(limit: int = 1, offset: int = 1):
+    return fake_trades[offset:][:limit]
+
+
+@app.post("/users/{user_id}")
+def change_user_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users))[0]
+    current_user["name"] = new_name
+    return {"status": 200, "data": current_user}
