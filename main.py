faade19d29c4195c8f304d282b6cf9349300cd80
nicola1223 <belchuk2005@mail.ru>
2024-03-22 22:45:49 +0300

    Added validators

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI(
     title="App"
@@ -12,7 +15,13 @@ fake_users = [
 ]
 
 
-@app.get("/users/{user_id}")
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+
+
+@app.get("/users/{user_id}", response_model=List[User])
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
 
@@ -23,13 +32,16 @@ fake_trades = [
 ]
 
 
-@app.get("/trades")
-def get_trades(limit: int = 1, offset: int = 1):
-    return fake_trades[offset:][:limit]
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
 
 
-@app.post("/users/{user_id}")
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get("id") == user_id, fake_users))[0]
-    current_user["name"] = new_name
-    return {"status": 200, "data": current_user}
+@app.post("/trades")
+def get_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
